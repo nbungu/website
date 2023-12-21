6711a83793ecd7e6abee24ae92afb1ed805b1b22
nbungu <arne.schunter@gmail.com>
2023-12-21 18:24:47 +0100

    NewsPost Text fully adapted to strapi text input

--- client/src/components/NewsPostContentElement.js
+++ client/src/components/NewsPostContentElement.js
@@ -10,14 +10,12 @@ function NewsPostContentElement({ newsPost }) {
   const imgPathRel = newsPost.attributes.titleimage?.data?.attributes.url;
   const imgPath = STRAPI_CMS_URL + imgPathRel;
   const imageCaption = newsPost.attributes.titleimagecaption;
-  const paragraphs = getParagraphs(newsPost.attributes.fulltext)
-  const imgCollection = getImageArray(newsPost.attributes.imagecollection)
+  const fullText = newsPost.attributes.fulltext;
+
+  const imgCollection = getImageArray(newsPost.attributes.imagecollection);
   const ytVideoUrl = newsPost.attributes.youtubeurl;
   
   // returns an array containing only the textblocks from the fullText Array of the JSON
-  function getParagraphs(fullTextArray) {
-    return fullTextArray?.map(item => item.children); // Children itself is also an array!
-  }
   function getImageArray(imageCollection) {
     return imageCollection?.data?.map(item => STRAPI_CMS_URL + item.attributes.url);
   }
@@ -26,11 +24,49 @@ function NewsPostContentElement({ newsPost }) {
     <div className="container-flex">  
       {imgPathRel ? <img className="img-fluid news-post-image rounded mt-2" src={imgPath} alt=''/> : <div className="m-0 p-0"/>}
       {imgPathRel && imageCaption ? <p className="my-2 text-body-secondary">{imageCaption}</p> : <div className="m-0 p-0"/>}
-      {paragraphs && paragraphs.map((child) => (
-        <p className="text-start mt-3">
-          {child.map(e => <span class={`${e.bold && 'fw-bold'} ${e.italic && 'fst-italic'}`}>{e.text}</span>)}
-        </p>
-      ))}
+
+      {fullText && fullText.map(textBlock => {
+        if (textBlock.type === 'paragraph') {
+          return (
+            <p className="text-start mt-3">
+              {textBlock.children.map(e => {
+                if (e.type === 'text') {
+                  return (
+                    <span className={`${e.bold && 'fw-bold'} ${e.italic && 'fst-italic'}`}>{e.text}</span>
+                  )
+                }
+                else if (e.type === 'link') { 
+                  return (
+                    <a href={e.url} className={`${e.children[0].bold && 'fw-bold'} ${e.children[0].italic && 'fst-italic'}`}>{e.children[0].text}</a>
+                  )
+                }
+              }
+              )}
+            </p>
+          );
+        }
+        else if (textBlock.type === 'heading') {
+          const HeadingComponent = `h${textBlock.level}`;
+          return (
+            <HeadingComponent className={`text-start mt-3`}>
+              {textBlock.children.map(e => {
+                if (e.type === 'text') {
+                  return (
+                    <span className={`${e.bold && 'fw-bold'} ${e.italic && 'fst-italic'}`}>{e.text}</span>
+                  )
+                }
+                else if (e.type === 'link') { 
+                  return (
+                    <a href={e.url} className={`${e.children[0].bold && 'fw-bold'} ${e.children[0].italic && 'fst-italic'}`}>{e.children[0].text}</a>
+                  )
+                }
+              }
+              )}
+            </HeadingComponent>
+          );
+        }
+        })
+      }
       {imgCollection && <div className="mt-5"><ImageGallery imagePaths={imgCollection}/></div>}
       {ytVideoUrl && <div className="mt-5"><YouTubeEmbed videoUrl={ytVideoUrl}/></div>}
     </div>
