4d41f25ed658dece6e11f0bb12e4ea30a29c779b
nbungu <arne.schunter@gmail.com>
2023-12-21 17:46:22 +0100

    NewsPost Content text with bold and italic feature

--- client/src/components/NewsPostContentElement.js
+++ client/src/components/NewsPostContentElement.js
@@ -10,23 +10,27 @@ function NewsPostContentElement({ newsPost }) {
   const imgPathRel = newsPost.attributes.titleimage?.data?.attributes.url;
   const imgPath = STRAPI_CMS_URL + imgPathRel;
   const imageCaption = newsPost.attributes.titleimagecaption;
-  const textBlocks = getTextBlocks(newsPost.attributes.fulltext)
+  const paragraphs = getParagraphs(newsPost.attributes.fulltext)
   const imgCollection = getImageArray(newsPost.attributes.imagecollection)
   const ytVideoUrl = newsPost.attributes.youtubeurl;
   
   // returns an array containing only the textblocks from the fullText Array of the JSON
-  function getTextBlocks(fullTextArray) {
-    return fullTextArray?.map(item => item.children[0].text);
+  function getParagraphs(fullTextArray) {
+    return fullTextArray?.map(item => item.children); // Children itself is also an array!
   }
   function getImageArray(imageCollection) {
     return imageCollection?.data?.map(item => STRAPI_CMS_URL + item.attributes.url);
   }
 
   return (
-    <div className="container-flex">
+    <div className="container-flex">  
       {imgPathRel ? <img className="img-fluid news-post-image rounded mt-2" src={imgPath} alt=''/> : <div className="m-0 p-0"/>}
       {imgPathRel && imageCaption ? <p className="my-2 text-body-secondary">{imageCaption}</p> : <div className="m-0 p-0"/>}
-      {textBlocks && textBlocks.map((textBlock) => (<p className="text-start mt-3">{textBlock}</p>))}
+      {paragraphs && paragraphs.map((child) => (
+        <p className="text-start mt-3">
+          {child.map(e => <span class={`${e.bold && 'fw-bold'} ${e.italic && 'fst-italic'}`}>{e.text}</span>)}
+        </p>
+      ))}
       {imgCollection && <div className="mt-5"><ImageGallery imagePaths={imgCollection}/></div>}
       {ytVideoUrl && <div className="mt-5"><YouTubeEmbed videoUrl={ytVideoUrl}/></div>}
     </div>
