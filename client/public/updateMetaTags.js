b1d5c897c3c1930412ca6192455b9bed88a2ded4
nbungu <arne.schunter@gmail.com>
2024-01-06 22:29:10 +0100

    fix

--- client/public/updateMetaTags.js
+++ client/public/updateMetaTags.js
@@ -2,6 +2,20 @@
 
   // Function to fetch and parse XML from a file
   function fetchAndParseXML(xmlUrl, documentUrl) {
+    fetch(xmlUrl)
+        .then(response => response.text())
+        .then(xmlString => {
+          // Parse the XML string
+          var parser = new DOMParser();
+          var xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+          xmlDoc && console.log('xmlDoc created from: ', xmlUrl);
+          setTagsForCurrentPage(xmlDoc, documentUrl);
+        })
+        .catch(error => {
+          console.error('Error fetching or parsing XML:', error);
+        });
+  }
+  /*function fetchAndParseXML(xmlUrl, documentUrl) {
     return new Promise((resolve, reject) => {
       // Fetch the XML file
       fetch(xmlUrl)
@@ -18,33 +32,49 @@
           reject(error);
         });
     });
-  }
+  }*/
 
   // Function to process the parsed XML
   function setTagsForCurrentPage(xmlDoc, documentUrl) {
     if (!xmlDoc || !documentUrl) return;
+    console.log(`Searching available Tags for ${documentUrl} in sitemap.`);
     // Get all <url> elements
     var urlElements = xmlDoc.childNodes[0].getElementsByTagName('url');
     // Find DocumentURL in List and get the Information
     for (var i = 0; i < urlElements.length; i++) {
       var url = urlElements[i].getElementsByTagName('loc')[0].textContent;
       if (url === documentUrl) {        
-        tags = {
+        console.log('Found corresponding URL in sitemap.');
+        var tags = {
           title: urlElements[i].getElementsByTagName('title')[0].textContent,
           descr: urlElements[i].getElementsByTagName('descr')[0].textContent,
           image: urlElements[i].getElementsByTagName('image')[0].textContent
         };
-        console.log('found Tags');
+        // Access the tag elements
+        var title = document.querySelector('meta[property="og:title"]');
+        var description = document.querySelector('meta[property="og:description"]');
+        var imgPath = document.querySelector('meta[property="og:image"]');
+        var url = document.querySelector('meta[property="og:url"]');
+
+        // Check if tags are loaded
+        if (title) {
+          title.setAttribute('content', tags.title);
+          description.setAttribute('content', tags.descr);
+          imgPath.setAttribute('content', tags.image);
+          url.setAttribute('content', document.location);
+
+          console.log('Changed Tags in index.html');
+        }
         break;
       }
     }
   }
 
-  var tags = {
+  /*var tags = {
     title: "",
     descr: "",
     image: ""
-  };
+  };*/
 
   // Wait for the DOM to be fully loaded
   document.addEventListener('DOMContentLoaded', function () {
@@ -52,8 +82,10 @@
     var currentURL = document.location;
     var xmlURL = './sitemap.xml';
 
-    // Use the promise to ensure that the tags are available
     fetchAndParseXML(xmlURL, currentURL)
+
+    // Use the promise to ensure that the tags are available
+    /*fetchAndParseXML(xmlURL, currentURL)
       .then(() => {
         // Access the tag elements
         var tabTitle = document.querySelector('title');
@@ -75,5 +107,5 @@
       })
       .catch(error => {
         console.error('Error:', error);
-      });
+      });*/
   });
