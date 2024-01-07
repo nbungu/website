e75b660d394057ca1c55d97c499e4d21f5a0e30c
nbungu <arne.schunter@gmail.com>
2024-01-07 16:22:18 +0100

    added Logs to fix bug

--- client/public/updateMetaTags.js
+++ client/public/updateMetaTags.js
@@ -1,7 +1,8 @@
 
 
   // Function to fetch and parse XML from a file
-  function fetchAndParseXML(xmlUrl, documentUrl) {
+  function setMetaTagsFromXML(xmlUrl, documentUrl) {
+    // Fetch XML-File
     fetch(xmlUrl)
         .then(response => response.text())
         .then(xmlString => {
@@ -9,30 +10,13 @@
           var parser = new DOMParser();
           var xmlDoc = parser.parseFromString(xmlString, 'text/xml');
           xmlDoc && console.log('xmlDoc created from: ', xmlUrl);
+          // Set the Tags
           setTagsForCurrentPage(xmlDoc, documentUrl);
         })
         .catch(error => {
           console.error('Error fetching or parsing XML:', error);
         });
   }
-  /*function fetchAndParseXML(xmlUrl, documentUrl) {
-    return new Promise((resolve, reject) => {
-      // Fetch the XML file
-      fetch(xmlUrl)
-        .then(response => response.text())
-        .then(xmlString => {
-          // Parse the XML string
-          var parser = new DOMParser();
-          var xmlDoc = parser.parseFromString(xmlString, 'text/xml');
-          setTagsForCurrentPage(xmlDoc, documentUrl);
-          resolve();
-        })
-        .catch(error => {
-          console.error('Error fetching or parsing XML:', error);
-          reject(error);
-        });
-    });
-  }*/
 
   // Function to process the parsed XML
   function setTagsForCurrentPage(xmlDoc, documentUrl) {
@@ -43,6 +27,7 @@
     // Find DocumentURL in List and get the Information
     for (var i = 0; i < urlElements.length; i++) {
       var url = urlElements[i].getElementsByTagName('loc')[0].textContent;
+      console.log(`checked URL: ${url}`);
       if (url === documentUrl) {        
         console.log('Found corresponding URL in sitemap.');
         var tags = {
@@ -54,15 +39,12 @@
         var title = document.querySelector('meta[property="og:title"]');
         var description = document.querySelector('meta[property="og:description"]');
         var imgPath = document.querySelector('meta[property="og:image"]');
-        var url = document.querySelector('meta[property="og:url"]');
 
         // Check if tags are loaded
         if (title) {
           title.setAttribute('content', tags.title);
           description.setAttribute('content', tags.descr);
           imgPath.setAttribute('content', tags.image);
-          url.setAttribute('content', document.location);
-
           console.log('Changed Tags in index.html');
         }
         break;
@@ -70,42 +52,14 @@
     }
   }
 
-  /*var tags = {
-    title: "",
-    descr: "",
-    image: ""
-  };*/
-
   // Wait for the DOM to be fully loaded
   document.addEventListener('DOMContentLoaded', function () {
 
     var currentURL = document.location;
     var xmlURL = './sitemap.xml';
 
-    fetchAndParseXML(xmlURL, currentURL)
-
-    // Use the promise to ensure that the tags are available
-    /*fetchAndParseXML(xmlURL, currentURL)
-      .then(() => {
-        // Access the tag elements
-        var tabTitle = document.querySelector('title');
-        var title = document.querySelector('meta[property="og:title"]');
-        var description = document.querySelector('meta[property="og:description"]');
-        var imgPath = document.querySelector('meta[property="og:image"]');
-        var url = document.querySelector('meta[property="og:url"]');
-
-        // Check if tags are loaded
-        if (tabTitle) {
-          //tabTitle.textContent = tags.title;
-          title.setAttribute('content', tags.title);
-          description.setAttribute('content', tags.descr);
-          imgPath.setAttribute('content', tags.image);
-          url.setAttribute('content', document.location);
-
-          console.log('set attributes');
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });*/
+    // URL Tag can be set independently
+    document.querySelector('meta[property="og:url"]').setAttribute('content', currentURL);
+    // Set All the other Tags from XML
+    setMetaTagsFromXML(xmlURL, currentURL);
   });
